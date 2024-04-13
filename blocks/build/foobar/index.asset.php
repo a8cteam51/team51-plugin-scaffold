e228fe40abe4d75abfbf00e4365102d357abf57d
Antonius Hegyes <tony.hegyes@automattic.com>
2024-04-13 15:52:30 +0200

    Update and rebuild

--- blocks/build/foobar/index.asset.php
+++ blocks/build/foobar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '75e9a283af0384f378a6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '553cde39727485a1ad71');
